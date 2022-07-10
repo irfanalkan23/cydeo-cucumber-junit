7083a9925b68050dba906f0047590a8b113e467f
irfan <23pointalpha@gmail.com>
2022-07-10 13:09:40 +0300

    DAY 17 - PARAMETERIZATION - DATATABLES REVIEW webTableOrder.feature Order_StepDefinitions ViewAllOrdersPage

--- src/test/java/com/cydeo/pages/OrderPage.java
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class OrderPage extends BasePage {
 
     public OrderPage() {
@@ -31,5 +33,17 @@ public class OrderPage extends BasePage {
 
     @FindBy(name = "zip")
     public WebElement inputZip;
+
+    @FindBy(name = "card")
+    public List<WebElement> cardType;
+
+    @FindBy(name = "cardNo")
+    public WebElement cardNoInput;
+
+    @FindBy(name = "cardExp")
+    public WebElement cardExpInput;
+
+    @FindBy(css = "button[type='submit']")
+    public WebElement processOrderButton;
 
 }
